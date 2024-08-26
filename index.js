2a39edab0b43112ec4c8a27f41286a1da57c8564
KuzAnton8335 <ant2783@gmail.com>
2024-08-26 15:32:14 +0300

    вывод в реальном времени времени отправления автобусов

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ import path from "node:path";
 // подключаем url для работы с путями
 import url from "node:url";
 // импорт функции DateTime from luxon для работы с датами  и временем
-import { DateTime } from "luxon";
+import { DateTime, Duration } from "luxon";
+//импорт websocketserver из плагина ws
+import { WebSocketServer } from "ws";
 
 // конфигурируем сервер для работы с buses.json
 const __filename = url.fileURLToPath(import.meta.url);
@@ -36,10 +38,10 @@ const getNextDeparture = (firstDepartureTime, frequencyMinutes) => {
 	// текущее время
 	const now = DateTime.now().setZone(timeZone);
 	// время первого отправления на часы и минуты
-	const [hours, minutes] = firstDepartureTime.split(":").map(Number);
+	const [hour, minute] = firstDepartureTime.split(":").map(Number);
 	// время первого отправления
 	let departure = DateTime.now()
-		.set({ hours, minutes })
+		.set({ hour, minute, second: 0, millisecond: 0 })
 		.setZone(timeZone);
 
 	// сравнение now и departure
@@ -47,11 +49,11 @@ const getNextDeparture = (firstDepartureTime, frequencyMinutes) => {
 		departure = departure.plus({ minutes: frequencyMinutes });
 	}
 	// время окончания работы автовокзала
-	const endOfDay = DateTime.now().set({ hours: 23, minutes: 59, seconds: 59, }).setZone(timeZone);
+	const endOfDay = DateTime.now().set({ hour: 23, minute: 59, second: 59, }).setZone(timeZone);
 
 	// сравнение для начала рабочего дня
 	if (departure > endOfDay) {
-		departure = departure.startOf("day").plus({ days: 1 }).set({ hours, minutes })
+		departure = departure.startOf("day").plus({ days: 1 }).set({ hour, minute })
 	}
 	// цикл while пока now(текущее время) > departure(времени первого отправления)
 	while (now > departure) {
@@ -60,24 +62,25 @@ const getNextDeparture = (firstDepartureTime, frequencyMinutes) => {
 
 	// сравнение для начала рабочего дня
 	if (departure > endOfDay) {
-		departure = departure.startOf("day").plus({ days: 1 }).set({ hours, minutes })
+		departure = departure.startOf("day").plus({ days: 1 }).set({ hour, minute })
 	}
-
 	// возвращаем время и дату для отправки автобуса
 	return departure;
 }
 
-
-
 // обновленние данных о движении автобусов
 const sendUpdateData = async () => {
+	// данные об автобусах
 	const buses = await loadBuses();
+	// получение текущего времени для вычесления интервала времени
+	const now = DateTime.now().setZone(timeZone);
 	// обновление времени отправления автобуса и времени пути автобуса
 	const updatedBuses = buses.map((bus) => {
 		const nextDeparture = getNextDeparture(
 			bus.firstDepartureTime,
 			bus.frequencyMinutes);
-
+		// перезапись времени в текущий момент
+		const timeRemaining = Duration.fromMillis(nextDeparture.diff(now).toMillis());
 
 		// возврощаем время и дату для отправки автобуса
 		return {
@@ -85,6 +88,7 @@ const sendUpdateData = async () => {
 			nextDeparture: {
 				data: nextDeparture.toFormat('yyyy-MM-dd'),
 				time: nextDeparture.toFormat('HH:mm:ss'),
+				remaining: timeRemaining.toFormat('hh:mm:ss'),
 			}
 		}
 	});
@@ -96,7 +100,6 @@ const sortBuses = (buses) =>
 	[...buses].sort((a, b) => new Date(`${a.nextDeparture.data}T${a.nextDeparture.time}`) - new Date(`${b.nextDeparture.data}T${b.nextDeparture.time}`)
 	);
 
-
 // вывод запроса данных отправления автобусов
 app.get('/next-departure', async (req, res) => {
 	try {
@@ -107,8 +110,48 @@ app.get('/next-departure', async (req, res) => {
 	} catch (error) {
 		res.send('error')
 	}
+});
+
+//создание websoket сервера
+const wss = new WebSocketServer({ noServer: true });
+// коллекция сообщений от клиентов
+const clients = new Set();
+// соеденение с клиентами
+wss.on('connection', (ws) => {
+	console.log('websoket connection');
+	// добавление клиента в коллекцию
+	clients.add(ws);
+	// обновленние данных
+	const sendUpdates = async () => {
+		try {
+			// обновленные данные об отправлении
+			const updatedBuses = await sendUpdateData();
+			// сортировка по времени отправления автобусов
+			const sortedBuses = sortBuses(updatedBuses);
+			// отправление данных в websoket сервер
+			ws.send(JSON.stringify(sortedBuses));
+		} catch (error) {
+			// ошибка в работе сервера webbsocket
+			console.error(`Error websoket connection:${error}`)
+		}
+	}
+	// иртервал обновленния данных на 1 секунду
+	const intervalId = setInterval(sendUpdates, 1000);
+
+	// закрытия websoketa соединения
+	ws.on('close', () => {
+		console.log('websoket close');
+		clients.delete(ws);
+	})
 })
 
-app.listen(port, () => {
-	console.log("Server running on http://localhost:" + port);
+// возвращает сервер
+const server = app.listen(port, () => {
+	console.log(`Server running on http://localhost:${port}`);
+})
+// обновление сервера
+server.on('upgrade', (req, socket, head) => {
+	wss.handleUpgrade(req, socket, head, (ws) => {
+		wss.emit('connection', ws, req);
+	});
 })
--- public/js/app.js
+++ public/js/app.js
@@ -20,6 +20,15 @@ const formateDate = (date) => date.toISOString().split('T')[0];
 // форматирование времени
 const formateTime = (date) => date.toTimeString().split(" ")[0].slice(0, 5);
 
+const getTimeRemainingSeconds = (departureTime) => {
+	// новые данные по дате
+	const now = new Date();
+	// получаем разницу времени
+	const timeDeference = departureTime - now;
+	//   вернем время в секундах
+	return Math.floor(timeDeference / 1000);
+}
+
 // отрисовка таблицы
 const renderBusData = (buses) => {
 	// получение таблицы
@@ -32,16 +41,55 @@ const renderBusData = (buses) => {
 		const row = document.createElement('tr');
 		// реальное время и дата
 		const nextDepartureDateTimeUTC = new Date(`${bus.nextDeparture.data}T${bus.nextDeparture.time}Z`,);
+
+		const remainingSeconds = getTimeRemainingSeconds(nextDepartureDateTimeUTC);
+		const remaningTimeText = remainingSeconds < 60 ? "Отправляется" : bus.nextDeparture.remaining
+
+
 		// заполняем строку таблицы
 		row.innerHTML = `
 			<td>${bus.busNumber}</td>
 			<td>${bus.startPoint} - ${bus.endPoint}</td>
 			<td>${formateDate(nextDepartureDateTimeUTC)}</td>
 			<td>${formateTime(nextDepartureDateTimeUTC)}</td>
+			<td>${remaningTimeText}</td>
 		`;
 		tableBody.appendChild(row);
 	})
+}
 
+const initWebsoket = () => {
+	// обращение к локальному хосту сервера websoket
+	const ws = new WebSocket(`ws://${location.host}`);
+	// обработка события "open" соединения (открытия сервера websoket)
+	ws.addEventListener('open', () => {
+		console.log("WebSocket connection is opened");
+	});
+	// обработка сообщений от websoket
+	ws.addEventListener('message', (event) => {
+		const buses = JSON.parse(event.data);
+		renderBusData(buses);
+	})
+	// обработка сообщений от websoket на случай ошибки в webbsocket
+	ws.addEventListener('error', (error) => {
+		console.error(`WebSocket error: ${error}`);
+	})
+	// закрытие соединения от websoket
+	ws.addEventListener('close', () => {
+		console.error(`WebSocket connection close`);
+	})
+}
+
+// обновление времени
+const updateTime = () => {
+	// элемент текущего времени
+	const currentTimeElement = document.getElementById("current-time");
+	// новые данные по дате
+	const now = new Date();
+	// вывод текущего времени
+	currentTimeElement.textContent = now.toTimeString().split(" ")[0];
+	// обновление времени через секунду
+	setTimeout(updateTime, 1000);
 }
 
 // инициализация проекта на клиенте
@@ -49,5 +97,9 @@ const init = async () => {
 	// данные об автобусах для отрисовки таблицы
 	const buses = await fetchBusData();
 	renderBusData(buses);
+	// загрузка сервера websoket
+	initWebsoket();
+	// обновление времени
+	updateTime();
 }
 init();
